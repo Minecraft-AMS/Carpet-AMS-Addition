2d31c44568904bb90901c48d6fbafed48e958dfe
1024_byte <77058327+1024-byteeeee@users.noreply.github.com>
2022-08-30 17:36:01 +0800

    Add files via upload

--- src/main/java/club/mcams/carpet/mixin/CactusfarmMixin.java
+++ src/main/java/club/mcams/carpet/mixin/CactusfarmMixin.java
@@ -18,18 +18,30 @@ import java.util.Random;
 
 @Mixin(CactusBlock.class)
 public abstract class CactusfarmMixin extends Block {
-
     public CactusfarmMixin(Settings settings) {
         super(settings);
     }
-
-    @Shadow
-    public abstract void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random);
-
-    @Inject(method = "scheduledTick", at = @At("TAIL"))
-    private void Mixin(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        if (AmsServerSettings.Cactusfarm){
-            randomTick(state, world, pos, random);
+    @Shadow public abstract void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random);
+    @Inject(
+            method = "scheduledTick",
+            at = @At(
+                    value = "INVOKE",
+                    shift = At.Shift.AFTER,
+                    target = "Lnet/minecraft/server/world/ServerWorld;breakBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"
+            ),
+            cancellable = true
+    )
+    private void mixin1(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (AmsServerSettings.Cactusfarm)
+            ci.cancel();
+    }
+    @Inject(
+            method = "scheduledTick",
+            at = @At("TAIL")
+    )
+    private void mixin2(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (AmsServerSettings.Cactusfarm) {
+            this.randomTick(state, world, pos, random);
         }
     }
 }
