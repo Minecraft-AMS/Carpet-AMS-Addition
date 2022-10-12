6f23fe152b799d4d9ef2909c35992bdf4f30cc15
1024_byte <77058327+1024-byteeeee@users.noreply.github.com>
2022-10-13 02:20:28 +0800

    Add files via upload

---
+++ src/main/java/club/mcams/carpet/mixin/zeroTick/SugarCaneBlockMixin.java
@@ -0,0 +1,49 @@
+package club.mcams.carpet.mixin.zeroTick;
+
+import club.mcams.carpet.AmsServerSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(SugarCaneBlock.class)
+public abstract class SugarCaneBlockMixin extends Block {
+
+    public SugarCaneBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Shadow
+    public abstract void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random);
+
+    @Inject(
+            method = "scheduledTick",
+            at = @At(
+                    value = "INVOKE",
+                    shift = At.Shift.AFTER,
+                    target = "Lnet/minecraft/server/world/ServerWorld;breakBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"
+            ),
+            cancellable = true
+    )
+    private void scheduleTick_mixin1(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (AmsServerSettings.zeroTickSugarCane)
+            ci.cancel();
+    }
+
+    @Inject(
+            method = "scheduledTick",
+            at = @At("TAIL")
+    )
+    private void scheduleTick_mixin2(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (AmsServerSettings.zeroTickSugarCane)
+            this.randomTick(state, world, pos, random);
+    }
+}
