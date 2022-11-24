f91d5df848750f70ecf08c1d21620af1992d2b13
wendavid552 <david.jianwei.wen@gmail.com>
2022-11-24 18:31:29 +0800

    Fix Messenger version incompatibility.

--- src/main/java/club/mcams/carpet/util/Messenger.java
+++ src/main/java/club/mcams/carpet/util/Messenger.java
@@ -2,6 +2,9 @@ package club.mcams.carpet.util;
 //#if MC >= 11600 && MC < 11900
 //$$ import net.minecraft.util.Util;
 //#endif
+//#if MC >= 11900
+//$$ import net.minecraft.text.Text
+//#endif
 
 import net.minecraft.text.BaseText;
 import net.minecraft.text.LiteralText;
