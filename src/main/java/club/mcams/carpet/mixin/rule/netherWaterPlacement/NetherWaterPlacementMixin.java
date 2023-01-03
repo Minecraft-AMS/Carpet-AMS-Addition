63b840bf04193b72a0528d42d435b24104ad37cc
1024_byte <77058327+1024-byteeeee@users.noreply.github.com>
2023-01-03 12:17:01 +0800

    做了一点小的调整

--- src/main/java/club/mcams/carpet/mixin/rule/netherWaterPlacement/NetherWaterPlacementMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/netherWaterPlacement/NetherWaterPlacementMixin.java
@@ -4,25 +4,17 @@ import club.mcams.carpet.AmsServerSettings;
 
 import net.minecraft.world.dimension.DimensionType;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DimensionType.class)
 public abstract class NetherWaterPlacementMixin {
-
-    @Final
-    @Shadow
-    private boolean ultrawarm;
-
-    /**
-     * @author
-     * @reason
-     */
-
-    @Overwrite
-    public boolean isUltrawarm() {
-        return !AmsServerSettings.netherWaterPlacement && this.ultrawarm;
+    @Inject(method = "isUltrawarm", at = @At("TAIL"), cancellable = true)
+    public void isUltrawarm(CallbackInfoReturnable<Boolean> cir) {
+        if (AmsServerSettings.netherWaterPlacement) {
+            cir.setReturnValue(false);
+        }
     }
 }
