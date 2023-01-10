0eb862f4a9f63031baed735f508c4f96e9ca1bc2
1024_byte <77058327+1024-byteeeee@users.noreply.github.com>
2023-01-10 11:00:02 +0800

    感觉好一点

---
+++ src/main/java/club/mcams/carpet/settings/AmsRuleCategory.java
@@ -0,0 +1,25 @@
+package club.mcams.carpet.settings;
+
+public class AmsRuleCategory {
+    public static final String AMS = "AMS";
+    public static final String CRAFTING = "crafting";
+    public static final String AMS_MOVABLE = "AMS_movable";
+    public static final String AMS_CHUNKLOADER = "AMS_chunkLoader";
+
+    /**
+     * Rules with this {@link RuleCategory} will have a client-side
+     * counterpart, so they can be set independently without the server
+     * being Carpet's
+     */
+    public static final String BUGFIX = "bugfix";
+    public static final String SURVIVAL = "survival";
+    public static final String CREATIVE = "creative";
+    public static final String EXPERIMENTAL = "experimental";
+    public static final String OPTIMIZATION = "optimization";
+    public static final String FEATURE = "feature";
+    public static final String COMMAND = "command";
+    public static final String TNT = "tnt";
+    public static final String DISPENSER = "dispenser";
+    public static final String SCARPET = "scarpet";
+    public static final String CLIENT = "client";
+}
