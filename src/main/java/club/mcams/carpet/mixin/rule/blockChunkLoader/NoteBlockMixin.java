7fe0439ca806103a2d0f1c8c962a1a5d99b8ca5f
1024-byteeeee <2270484921@qq.com>
2023-01-16 10:38:56 +0800

    新增触发加载条件

--- src/main/java/club/mcams/carpet/mixin/rule/blockChunkLoader/NoteBlockMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/blockChunkLoader/NoteBlockMixin.java
@@ -3,21 +3,24 @@ package club.mcams.carpet.mixin.rule.blockChunkLoader;
 import club.mcams.carpet.AmsServerSettings;
 import club.mcams.carpet.function.BlockChunkLoader;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.NoteBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-//#if MC>=11903
-//$$import net.minecraft.block.BlockState;
-//#endif
 //#if MC>=11900
-//$$import net.minecraft.entity.Entity;
+//$$ import net.minecraft.block.BlockState;
+//$$ import net.minecraft.entity.Entity;
 //#endif
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
 
 @Mixin(NoteBlock.class)
 public abstract class NoteBlockMixin {
@@ -30,9 +33,16 @@ public abstract class NoteBlockMixin {
             //$$ BlockState blockState,
             //#endif
             World world, BlockPos pos, CallbackInfo info) {
-        if (AmsServerSettings.noteBlockChunkLoader && !world.isClient) {
+        if (Objects.equals(AmsServerSettings.noteBlockChunkLoader, "note_block") && !world.isClient) {
             ChunkPos chunkPos = new ChunkPos(pos);
             ((ServerWorld) world).getChunkManager().addTicket(BlockChunkLoader.BLOCK_LOADER, chunkPos, 3, chunkPos);
+        }
+        if (Objects.equals(AmsServerSettings.noteBlockChunkLoader, "bone_block") && !world.isClient) {
+            BlockState noteBlock = world.getBlockState(pos.up(1));
+            if (Objects.equals(AmsServerSettings.noteBlockChunkLoader, "bone_block") && noteBlock.isOf(Blocks.BONE_BLOCK)) {
+                ChunkPos chunkPos = new ChunkPos(pos.up(1));
+                ((ServerWorld) world).getChunkManager().addTicket(BlockChunkLoader.BLOCK_LOADER, chunkPos, 3, chunkPos);
+            }
         }
     }
 }
--- src/main/java/club/mcams/carpet/mixin/rule/blockChunkLoader/PistonBlockMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/blockChunkLoader/PistonBlockMixin.java
@@ -7,13 +7,10 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.FacingBlock;
 import net.minecraft.block.PistonBlock;
 import net.minecraft.block.BlockState;
-
 import net.minecraft.server.world.ServerWorld;
-
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
-
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,14 +18,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+
 @Mixin(PistonBlock.class)
 public abstract class PistonBlockMixin {
     @Inject(method = "onSyncedBlockEvent", at = @At("HEAD"))
-    private void load(BlockState state, World world, BlockPos pos, int type, int data, CallbackInfoReturnable<Boolean> cir) {
-        if(AmsServerSettings.pistonBlockChunkLoader && !world.isClient) {
+    private void onSyncedBlockEvent(BlockState state, World world, BlockPos pos, int type, int data, CallbackInfoReturnable<Boolean> cir) {
+        if(Objects.equals(AmsServerSettings.pistonBlockChunkLoader, "bone_block") && !world.isClient) {
             Direction direction = state.get(FacingBlock.FACING);
             BlockState pistonBlock = world.getBlockState(pos.up(1));
-            if (AmsServerSettings.pistonBlockChunkLoader && pistonBlock.isOf(Blocks.BONE_BLOCK)) {
+            if (Objects.equals(AmsServerSettings.pistonBlockChunkLoader, "bone_block") && pistonBlock.isOf(Blocks.BONE_BLOCK)) {
+                ChunkPos chunkPos = new ChunkPos(pos.offset(direction));
+                ((ServerWorld) world).getChunkManager().addTicket(BlockChunkLoader.BLOCK_LOADER, chunkPos, 3, chunkPos);
+            }
+        }
+        if(Objects.equals(AmsServerSettings.pistonBlockChunkLoader, "bedrock") && !world.isClient) {
+            Direction direction = state.get(FacingBlock.FACING);
+            BlockState pistonBlock = world.getBlockState(pos.down(1));
+            if (Objects.equals(AmsServerSettings.pistonBlockChunkLoader, "bedrock") && pistonBlock.isOf(Blocks.BEDROCK)) {
                 ChunkPos chunkPos = new ChunkPos(pos.offset(direction));
                 ((ServerWorld) world).getChunkManager().addTicket(BlockChunkLoader.BLOCK_LOADER, chunkPos, 3, chunkPos);
             }
