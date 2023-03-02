b747814cc91c9c381435b91174f4043d174afaf9
wendavid552 <david.jianwei.wen@gmail.com>
2023-03-02 13:49:30 +0800

    fix: version compat of PointedDripstoneBlock (for <1.17)

--- versions/1.16.5/src/main/java/club/mcams/carpet/mixin/rule/coarseDirtDriedToSand/IMixinPointedDripstoneBlock.java
+++ versions/1.16.5/src/main/java/club/mcams/carpet/mixin/rule/coarseDirtDriedToSand/IMixinPointedDripstoneBlock.java
@@ -1,4 +1,11 @@
 package club.mcams.carpet.mixin.rule.coarseDirtDriedToSand;
 
+import club.mcams.carpet.util.compat.DummyClass;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Restriction(require = @Condition(value = "minecraft", versionPredicates = "<1.17.0"))
+@Mixin(DummyClass.class)
 public interface IMixinPointedDripstoneBlock {
 }
