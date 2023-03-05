983434dfa87b6a2a3f1db2556b014bc89357a351
1024-byteeeee <2270484921@qq.com>
2023-03-05 13:53:41 +0800

    修复movableXXX系列规则在开启时尝试往Y-1或Y-65推动方块时能正常推出且消除方块的问题

--- src/main/java/club/mcams/carpet/mixin/rule/movableBlocks/PistonBlockMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/movableBlocks/PistonBlockMixin.java
@@ -18,34 +18,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class PistonBlockMixin {
     @Inject(method = "isMovable", at = @At("HEAD"), cancellable = true)
     private static void MovableBlocks(BlockState state, World world, BlockPos pos, Direction direction, boolean canBreak, Direction pistonDir, CallbackInfoReturnable<Boolean> cir) {
-        if (AmsServerSettings.movableEnderChest && state.isOf(Blocks.ENDER_CHEST)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableEndPortalFrame && state.isOf(Blocks.END_PORTAL_FRAME)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableObsidian && state.isOf(Blocks.OBSIDIAN)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableCryingObsidian && state.isOf(Blocks.CRYING_OBSIDIAN)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableBedRock && state.isOf(Blocks.BEDROCK)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableEnchantingTable && state.isOf(Blocks.ENCHANTING_TABLE)) {
-            cir.setReturnValue(true);
-        }
-        if (AmsServerSettings.movableBeacon && state.isOf(Blocks.BEACON)) {
-            cir.setReturnValue(true);
-        }
-        //#if MC>=11900
-        //$$if (AmsServerSettings.movableReinforcedDeepslate && state.isOf(Blocks.REINFORCED_DEEPSLATE)) {
-        //$$    cir.setReturnValue(true);
-        //$$}
-        //#endif
-        if (AmsServerSettings.movableAnvil && state.isOf(Blocks.ANVIL)) {
-            cir.setReturnValue(true);
+        if (
+                        (AmsServerSettings.movableEnderChest && state.isOf(Blocks.ENDER_CHEST)) ||
+                        (AmsServerSettings.movableEndPortalFrame && state.isOf(Blocks.END_PORTAL_FRAME)) ||
+                        (AmsServerSettings.movableObsidian && state.isOf(Blocks.OBSIDIAN)) ||
+                        (AmsServerSettings.movableCryingObsidian && state.isOf(Blocks.CRYING_OBSIDIAN)) ||
+                        (AmsServerSettings.movableBedRock && state.isOf(Blocks.BEDROCK)) ||
+                        (AmsServerSettings.movableEnchantingTable && state.isOf(Blocks.ENCHANTING_TABLE)) ||
+                        (AmsServerSettings.movableBeacon && state.isOf(Blocks.BEACON)) ||
+                        (AmsServerSettings.movableAnvil && state.isOf(Blocks.ANVIL))
+            //#if MC>=11900
+            //$$ || (AmsServerSettings.movableReinforcedDeepslate && state.isOf(Blocks.REINFORCED_DEEPSLATE))
+            //#endif
+        ) {
+            if (direction == Direction.DOWN && pos.getY() == world.getBottomY()) {
+                cir.setReturnValue(false);
+            } else if (direction == Direction.UP && pos.getY() == world.getTopY() - 1) {
+                cir.setReturnValue(false);
+            } else {
+                cir.setReturnValue(true);
+            }
         }
     }
 }
