a348fb103495f31c09a92883cef798f421733903
1024-byteeeee <2270484921@qq.com>
2023-03-19 23:26:33 +0800

    兼容1.19.4

--- src/main/java/club/mcams/carpet/mixin/rule/safeFlight/ServerPlayerEntityMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/safeFlight/ServerPlayerEntityMixin.java
@@ -3,6 +3,9 @@ package club.mcams.carpet.mixin.rule.safeFlight;
 import club.mcams.carpet.AmsServerSettings;
 
 import net.minecraft.entity.damage.DamageSource;
+//#if MC>=11900
+//$$ import net.minecraft.entity.damage.DamageTypes;
+//#endif
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,8 +17,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ServerPlayerEntityMixin {
     @Inject(method = "isInvulnerableTo",at = @At("TAIL"), cancellable = true)
     public void isInvulnerableTo(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+        //#if MC>11900
+        //$$ if(AmsServerSettings.safeFlight && damageSource.isOf(DamageTypes.FLY_INTO_WALL)) {
+        //#else
         if(AmsServerSettings.safeFlight && damageSource.equals(DamageSource.FLY_INTO_WALL)) {
+            //#endif
             cir.setReturnValue(true);
+            cir.cancel();
         }
     }
 }
