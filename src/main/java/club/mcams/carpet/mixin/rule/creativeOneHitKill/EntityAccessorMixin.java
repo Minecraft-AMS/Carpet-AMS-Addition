7e4c19867776a5ecf88bb6c608580dd8f4b20a9a
1024-byteeeee <2270484921@qq.com>
2023-03-25 02:28:13 +0800

    新增规则：创造模式一击必杀

---
+++ src/main/java/club/mcams/carpet/mixin/rule/creativeOneHitKill/EntityAccessorMixin.java
@@ -0,0 +1,26 @@
+package club.mcams.carpet.mixin.rule.creativeOneHitKill;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Entity.class)
+public interface EntityAccessorMixin
+{
+    @Accessor("world")
+    World accessorGetWorld();
+
+    @Invoker("getX")
+    double invokerGetX();
+
+    @Invoker("getY")
+    double invokerGetY();
+
+    @Invoker("getZ")
+    double invokerGetZ();
+
+    @Invoker("isSneaking")
+    boolean invokerIsSneaking();
+}
