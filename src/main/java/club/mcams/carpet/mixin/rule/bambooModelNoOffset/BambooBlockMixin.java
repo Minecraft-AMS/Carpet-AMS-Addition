8a87768e8fdca53c6367cc86a68a44a09bb78216
1024-byteeeee <2270484921@qq.com>
2023-04-05 22:12:33 +0800

    兼容1.19.4且让竹苗也没有方块偏移

--- src/main/java/club/mcams/carpet/mixin/rule/bambooModelNoOffset/BambooBlockMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/bambooModelNoOffset/BambooBlockMixin.java
@@ -1,21 +1,25 @@
 package club.mcams.carpet.mixin.rule.bambooModelNoOffset;
 
 import club.mcams.carpet.AmsServerSettings;
-
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.BambooBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BambooBlock.class)
+@Mixin(net.minecraft.block.AbstractBlock.AbstractBlockState.class)
 public abstract class BambooBlockMixin {
-    @Inject(method = "getOffsetType", at = @At("TAIL"), cancellable = true)
-    public void getOffsetType(CallbackInfoReturnable<AbstractBlock.OffsetType> cir) {
-        if(AmsServerSettings.bambooModelNoOffset) {
-            cir.setReturnValue(AbstractBlock.OffsetType.NONE);
+    @Shadow public abstract Block getBlock();
+    @Inject(method = "getModelOffset(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/Vec3d;", at = @At("HEAD"), cancellable = true)
+    public void getModelOffset(BlockView world, BlockPos pos, CallbackInfoReturnable<Vec3d> cir) {
+        if(AmsServerSettings.bambooModelNoOffset && (this.getBlock() == Blocks.BAMBOO || this.getBlock() == Blocks.BAMBOO_SAPLING)) {
+            cir.setReturnValue(Vec3d.ZERO);
             cir.cancel();
         }
     }
