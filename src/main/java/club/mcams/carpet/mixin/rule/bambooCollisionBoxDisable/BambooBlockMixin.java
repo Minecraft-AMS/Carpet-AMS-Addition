f4073742b2b912587106b2878a35631c96556fb5
1024-byteeeee <2270484921@qq.com>
2023-04-08 01:42:42 +0800

    添加竹子无碰撞箱规则

---
+++ src/main/java/club/mcams/carpet/mixin/rule/bambooCollisionBoxDisable/BambooBlockMixin.java
@@ -0,0 +1,27 @@
+package club.mcams.carpet.mixin.rule.bambooCollisionBoxDisable;
+
+import club.mcams.carpet.AmsServerSettings;
+
+import net.minecraft.block.BambooBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BambooBlock.class)
+public abstract class BambooBlockMixin {
+    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
+    public void getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if(AmsServerSettings.bambooCollisionBoxDisabled) {
+            cir.setReturnValue(VoxelShapes.empty());
+            cir.cancel();
+        }
+    }
+}
