22f827b24d341cd12dcb683c45d2465f57484b14
1024-byteeeee <2270484921@qq.com>
2023-11-03 17:00:55 +0800

    :white_check_mark: 添加@Unique注解

--- src/main/java/club/mcams/carpet/mixin/rule/largeEnderChest/PlayerEntityMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/largeEnderChest/PlayerEntityMixin.java
@@ -31,12 +31,14 @@ import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
+	@Unique
 	private static final int EXPANDED_ENDERCHEST_SIZE = 9 * 6;
 
 	@Shadow
