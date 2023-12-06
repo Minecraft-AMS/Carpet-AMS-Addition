5ecb3d4246689c142943f459f14f278c7acba79c
1024_byteeeee <2270484921@qq.com>
2023-12-07 02:47:07 +0800

    :white_check_mark: 新增规则：`regeneratingDragonEgg`

---
+++ src/main/java/club/mcams/carpet/mixin/rule/regeneratingDragonEgg/EnderDragonFightMixin.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the Carpet AMS Addition project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2023  A Minecraft Server and contributors
+ *
+ * Carpet AMS Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpet AMS Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpet AMS Addition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package club.mcams.carpet.mixin.rule.regeneratingDragonEgg;
+
+import club.mcams.carpet.AmsServerSettings;
+
+import net.minecraft.block.Blocks;
+//#if MC>=12000
+//$$ import net.minecraft.util.math.BlockPos;
+//#endif
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.gen.feature.EndPortalFeature;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EnderDragonFight.class)
+public abstract class EnderDragonFightMixin {
+
+    //#if MC>=12000
+    //$$ @Shadow
+    //$$ @Final
+    //$$private BlockPos origin;
+    //#endif
+
+    @Shadow
+    @Final
+    private ServerWorld world;
+
+    @Shadow
+    private boolean previouslyKilled;
+
+    @Inject(method = "dragonKilled", at = @At("HEAD"))
+    private void dragonKilled(EnderDragonEntity dragon, CallbackInfo ci) {
+        if (AmsServerSettings.regeneratingDragonEgg && this.previouslyKilled) {
+            //#if MC>=12000
+            //$$ this.world.setBlockState(this.world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, EndPortalFeature.offsetOrigin(this.origin)), Blocks.DRAGON_EGG.getDefaultState());
+            //#else
+            this.world.setBlockState(this.world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, EndPortalFeature.ORIGIN), Blocks.DRAGON_EGG.getDefaultState());
+            //#endif
+        }
+    }
+}
