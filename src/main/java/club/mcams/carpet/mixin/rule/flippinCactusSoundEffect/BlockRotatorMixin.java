c1b23f3c50209ee444ec226e095934d019915ec2
1024_byteeeee <2270484921@qq.com>
2024-01-09 04:18:17 +0800

    :white_check_mark: 拓展规则

--- src/main/java/club/mcams/carpet/mixin/rule/flippinCactusSoundEffect/BlockRotatorMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/flippinCactusSoundEffect/BlockRotatorMixin.java
@@ -42,11 +42,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class BlockRotatorMixin {
     @SuppressWarnings("EnhancedSwitchMigration")
     @Inject(
-            //#if MC>11800
-            //$$ method = "flipBlock",
-            //#else
             method = "flip_block",
-            //#endif
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"
@@ -54,23 +50,38 @@ public abstract class BlockRotatorMixin {
     )
     private static void flipBlock(BlockState state, World world, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
         if (AmsServerSettings.flippinCactusSoundEffect != 0) {
-            SoundEvent soundEvents = SoundEvents.BLOCK_LEVER_CLICK;
-            float volume = 0.0F, pitch = 0.0F;
+            float volume, pitch;
+            SoundEvent leverClick = SoundEvents.BLOCK_LEVER_CLICK;
+            SoundEvent villagerAmbient = SoundEvents.ENTITY_VILLAGER_AMBIENT;
+            SoundEvent endermanTeleport = SoundEvents.ENTITY_ENDERMAN_TELEPORT;
+            SoundCategory blocks = SoundCategory.BLOCKS;
+            SoundCategory players = SoundCategory.PLAYERS;
             switch (AmsServerSettings.flippinCactusSoundEffect) {
                 case 1:
                     volume = 1.0F;
                     pitch = 0.95F;
+                    player.playSound(leverClick, blocks, volume, pitch);
                     break;
                 case 2:
                     volume = 1.0F;
                     pitch = 2.5F;
+                    player.playSound(leverClick, blocks, volume, pitch);
                     break;
                 case 3:
                     volume = 1.0F;
                     pitch = 0.75F;
+                    player.playSound(leverClick, blocks, volume, pitch);
                     break;
+                case 4:
+                    volume = 1.0F;
+                    pitch = 1.0F;
+                    player.playSound(villagerAmbient, players, volume, pitch);
+                    break;
+                case 5:
+                    volume = 1.0F;
+                    pitch = 1.0F;
+                    player.playSound(endermanTeleport, players, volume, pitch);
             }
-            player.playSound(soundEvents, SoundCategory.BLOCKS, volume, pitch);
         }
     }
 }
