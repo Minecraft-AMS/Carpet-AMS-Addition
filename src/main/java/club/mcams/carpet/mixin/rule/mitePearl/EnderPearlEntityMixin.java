0f1a762a2f87901e90f48167d1ef5ee8116a4049
1024_byteeeee <2270484921@qq.com>
2024-02-24 07:02:03 +0800

    :art: 调整代码格式

--- src/main/java/club/mcams/carpet/mixin/rule/mitePearl/EnderPearlEntityMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/mitePearl/EnderPearlEntityMixin.java
@@ -27,9 +27,9 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.EndermiteEntity;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
-
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
