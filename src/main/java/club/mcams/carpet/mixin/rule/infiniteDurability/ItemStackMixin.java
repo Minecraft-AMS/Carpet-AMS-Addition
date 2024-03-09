1a913e1fd5124332349f23643ea11e340806f132
1024_byteeeee <2270484921@qq.com>
2024-03-09 12:24:36 +0800

    :bug: 修复pr #94的版本差异问题

--- src/main/java/club/mcams/carpet/mixin/rule/infiniteDurability/ItemStackMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/infiniteDurability/ItemStackMixin.java
@@ -21,19 +21,26 @@
 package club.mcams.carpet.mixin.rule.infiniteDurability;
 
 import club.mcams.carpet.AmsServerSettings;
+
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Random;
-
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
-    @Inject(method = "damage(ILjava/util/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z", at = @At("HEAD"), cancellable = true)
-    private void cancelDamage(int amount, Random random, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(
+        //#if MC>=11900
+        //$$ method = "damage(ILnet/minecraft/util/math/random/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z",
+        //#else
+        method = "damage(ILjava/util/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z",
+        //#endif
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void damage(CallbackInfoReturnable<Boolean> cir) {
         if (AmsServerSettings.infiniteDurability) {
             cir.setReturnValue(false);
         }
