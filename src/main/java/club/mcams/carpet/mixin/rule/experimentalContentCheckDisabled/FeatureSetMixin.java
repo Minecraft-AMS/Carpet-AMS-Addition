59ceadfa252c521de8e11b2731d89594c122a940
1024_byteeeee <2270484921@qq.com>
2024-03-23 18:27:22 +0800

    :white_check_mark: 代码调整

--- src/main/java/club/mcams/carpet/mixin/rule/experimentalContentCheckDisabled/FeatureSetMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/experimentalContentCheckDisabled/FeatureSetMixin.java
@@ -2,7 +2,7 @@
  * This file is part of the Carpet AMS Addition project, licensed under the
  * GNU Lesser General Public License v3.0
  *
- * Copyright (C) 2024  A Minecraft Server and contributors
+ * Copyright (C) 2024 A Minecraft Server and contributors
  *
  * Carpet AMS Addition is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -11,22 +11,22 @@
  *
  * Carpet AMS Addition is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Carpet AMS Addition.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Carpet AMS Addition. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package club.mcams.carpet.mixin.rule.experimentalContentCheckDisabled;
-
-import club.mcams.carpet.utils.compat.DummyClass;
 
 //#if MC>=11900
 //$$ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 //$$ import net.minecraft.resource.featuretoggle.FeatureSet;
 //$$ import org.spongepowered.asm.mixin.injection.At;
 //$$ import club.mcams.carpet.AmsServerSettings;
+//#else
+import club.mcams.carpet.utils.compat.DummyClass;
 //#endif
 
 import org.spongepowered.asm.mixin.Mixin;
