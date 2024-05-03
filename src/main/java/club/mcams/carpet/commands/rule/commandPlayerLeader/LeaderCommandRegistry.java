388ed678d2a7fff5cad8b7d0c8725fb699268c7e
1024_byteeeee <2270484921@qq.com>
2024-05-03 10:31:17 +0800

    :white_check_mark: 完善/leader

--- src/main/java/club/mcams/carpet/commands/rule/commandPlayerLeader/LeaderCommandRegistry.java
+++ src/main/java/club/mcams/carpet/commands/rule/commandPlayerLeader/LeaderCommandRegistry.java
@@ -24,6 +24,7 @@ import club.mcams.carpet.AmsServerSettings;
 import club.mcams.carpet.translations.Translator;
 import club.mcams.carpet.utils.CommandHelper;
 import club.mcams.carpet.utils.Messenger;
+import club.mcams.carpet.config.rule.commandLeader.LeaderConfig;
 
 import com.mojang.brigadier.CommandDispatcher;
 
@@ -37,13 +38,20 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Style;
 import net.minecraft.util.Formatting;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class LeaderCommandRegistry {
     private static final Translator translator = new Translator("command.leader");
+    private static final String MSG_HEAD = "<commandPlayerLeader> ";
     private static final int GLOWING_TIME = Integer.MAX_VALUE;
-    private static final StatusEffectInstance HIGH_LIGHT = new StatusEffectInstance(StatusEffects.GLOWING, GLOWING_TIME);
+    public static final StatusEffectInstance HIGH_LIGHT = new StatusEffectInstance(StatusEffects.GLOWING, GLOWING_TIME);
+    public static final Map<String, String> LEADER_LIST = new HashMap<>();
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
@@ -55,38 +63,100 @@ public class LeaderCommandRegistry {
             .then(literal("remove")
             .then(argument("player", EntityArgumentType.player())
             .executes(context -> remove(context.getSource().getServer(), EntityArgumentType.getPlayer(context, "player")))))
+            .then(literal("removeAll")
+            .executes(context -> removeAll(context.getSource().getServer(), context.getSource().getPlayer())))
+            .then(literal("list")
+            .executes(context -> list(context.getSource().getPlayer())))
             .then(literal("help")
             .executes(context -> help(context.getSource().getPlayer())))
         );
     }
 
     private static int add(MinecraftServer server, PlayerEntity targetPlayer) {
-        targetPlayer.addStatusEffect(HIGH_LIGHT);
-        Messenger.sendServerMessage(
-            server,
-            Messenger.s(
-                String.format("%s %s", getPlayerName(targetPlayer), translator.tr("add").getString())
-            ).setStyle(Style.EMPTY.withColor(Formatting.GRAY))
-        );
+        if (!LEADER_LIST.containsValue(getPlayerUUID(targetPlayer))) {
+            targetPlayer.addStatusEffect(HIGH_LIGHT);
+            Messenger.sendServerMessage(
+                server,
+                Messenger.s(
+                    String.format("%s %s %s", MSG_HEAD, getPlayerName(targetPlayer), translator.tr("add").getString())
+                ).formatted(Formatting.GRAY)
+            );
+            LEADER_LIST.put(getPlayerName(targetPlayer), getPlayerUUID(targetPlayer));
+            saveToJson(server);
+        } else {
+            Messenger.sendServerMessage(
+                server,
+                Messenger.s(
+                    MSG_HEAD + getPlayerName(targetPlayer) + " " + translator.tr("is_already_leader").getString()
+                ).formatted(Formatting.RED, Formatting.ITALIC)
+            );
+        }
         return 1;
     }
 
     private static int remove(MinecraftServer server, PlayerEntity targetPlayer) {
-        targetPlayer.removeStatusEffect(HIGH_LIGHT.getEffectType());
-        Messenger.sendServerMessage(
-            server,
-            Messenger.s(
-                String.format("%s %s", getPlayerName(targetPlayer), translator.tr("remove").getString())
-            ).setStyle(Style.EMPTY.withColor(Formatting.GRAY))
+        if (LEADER_LIST.containsValue(getPlayerUUID(targetPlayer))) {
+            targetPlayer.removeStatusEffect(HIGH_LIGHT.getEffectType());
+            Messenger.sendServerMessage(
+                server,
+                Messenger.s(
+                    String.format("%s %s %s", MSG_HEAD, getPlayerName(targetPlayer), translator.tr("remove").getString())
+                ).formatted(Formatting.GRAY)
+            );
+            LEADER_LIST.remove(getPlayerName(targetPlayer), getPlayerUUID(targetPlayer));
+            saveToJson(server);
+        } else {
+            Messenger.sendServerMessage(
+                server,
+                Messenger.s(
+                    String.format(MSG_HEAD + getPlayerName(targetPlayer) + " " + translator.tr("is_not_leader").getString())
+                ).formatted(Formatting.RED, Formatting.ITALIC)
+            );
+        }
+        return 1;
+    }
+
+    private static int removeAll(MinecraftServer server, PlayerEntity player) {
+        Iterator<Map.Entry<String, String>> iterator = LEADER_LIST.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+            String playerUUID = entry.getValue();
+            PlayerEntity targetPlayer = server.getPlayerManager().getPlayer(UUID.fromString(playerUUID));
+            if (targetPlayer != null) {
+                targetPlayer.removeStatusEffect(HIGH_LIGHT.getEffectType());
+            }
+            iterator.remove();
+        }
+        LEADER_LIST.clear();
+        saveToJson(server);
+        player.sendMessage(
+            Messenger.s(MSG_HEAD + translator.tr("removeAll").getString()).formatted(Formatting.YELLOW), false
         );
+        return 1;
+    }
+
+    private static int list(PlayerEntity player) {
+        final String LINE = "-----------------------------------------";
+        player.sendMessage(
+            Messenger.s(
+                translator.tr("list").getString() + "\n" + LINE
+            ).formatted(Formatting.AQUA, Formatting.BOLD), false
+        );
+        for (Map.Entry<String, String> entry : LEADER_LIST.entrySet()) {
+            String playerName = entry.getKey();
+            String playerUUID = getPlayerUUID(player);
+            player.sendMessage(Messenger.s(playerName + " - " + playerUUID).formatted(Formatting.DARK_AQUA), false);
+        }
         return 1;
     }
 
     private static int help(PlayerEntity player) {
         String addHelp = translator.tr("help.add").getString();
         String removeHelp = translator.tr("help.remove").getString();
+        String removeAllHelp = translator.tr("help.removeAll").getString();
+        String listHelp = translator.tr("help.list").getString();
         player.sendMessage(
-            Messenger.s(addHelp + "\n" + removeHelp).
+            Messenger.s(addHelp + "\n" + removeHelp + "\n" + removeAllHelp + "\n" + listHelp).
             setStyle(Style.EMPTY.withColor(Formatting.GRAY)),
             false
         );
@@ -95,5 +165,14 @@ public class LeaderCommandRegistry {
 
     private static String getPlayerName(PlayerEntity player) {
         return player.getName().getString();
+    }
+
+    private static String getPlayerUUID(PlayerEntity player) {
+        return player.getUuidAsString();
+    }
+
+    private static void saveToJson(MinecraftServer server) {
+        String CONFIG_FILE_PATH = LeaderConfig.getPath(server);
+        LeaderConfig.saveToJson(LEADER_LIST, CONFIG_FILE_PATH);
     }
 }
---
+++ src/main/java/club/mcams/carpet/config/rule/commandLeader/LeaderConfig.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of the Carpet AMS Addition project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2024 A Minecraft Server and contributors
+ *
+ * Carpet AMS Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpet AMS Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpet AMS Addition. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package club.mcams.carpet.config.rule.commandLeader;
+
+import club.mcams.carpet.AmsServer;
+import club.mcams.carpet.commands.rule.commandPlayerLeader.LeaderCommandRegistry;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.WorldSavePath;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeaderConfig {
+    @SuppressWarnings("ReadWriteStringCanBeUsed")
+    public static void loadFromJson(String configFilePath) {
+        Gson gson = new Gson();
+        Path path = Paths.get(configFilePath);
+        LeaderCommandRegistry.LEADER_LIST.clear();
+        if (Files.exists(path)) {
+            try {
+                String json = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+                Type type = new TypeToken<Map<String, String>>() {}.getType();
+                Map<String, String> simplifiedMap = gson.fromJson(json, type);
+                LeaderCommandRegistry.LEADER_LIST.putAll(simplifiedMap);
+            } catch (IOException e) {
+                AmsServer.LOGGER.warn("Failed to load config", e);
+            }
+        }
+    }
+
+    @SuppressWarnings("ReadWriteStringCanBeUsed")
+    public static void saveToJson(Map<String, String> customBlockMap, String configFilePath) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Map<String, String> simplifiedMap = new HashMap<>(customBlockMap);
+        String json = gson.toJson(simplifiedMap);
+        try {
+            Path path = Paths.get(configFilePath);
+            Files.createDirectories(path.getParent());
+            Files.write(path, json.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            AmsServer.LOGGER.warn("Failed to save config", e);
+        }
+    }
+
+    public static String getPath(MinecraftServer server) {
+        return server.getSavePath(WorldSavePath.ROOT).resolve("carpetamsaddition/leader" + ".json").toString();
+    }
+}
