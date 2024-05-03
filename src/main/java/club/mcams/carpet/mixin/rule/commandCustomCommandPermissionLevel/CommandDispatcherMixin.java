3e4764c6ce178e68879662f628afe4dff7e7c2ea
1024_byteeeee <2270484921@qq.com>
2024-05-03 23:34:29 +0800

    :zap: 需要判断是否开启规则

--- src/main/java/club/mcams/carpet/mixin/rule/commandCustomCommandPermissionLevel/CommandDispatcherMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/commandCustomCommandPermissionLevel/CommandDispatcherMixin.java
@@ -20,6 +20,7 @@
 
 package club.mcams.carpet.mixin.rule.commandCustomCommandPermissionLevel;
 
+import club.mcams.carpet.AmsServerSettings;
 import club.mcams.carpet.commands.rule.commandCustomCommandPermissionLevel.CustomCommandPermissionLevelRegistry;
 
 import com.mojang.brigadier.CommandDispatcher;
@@ -33,11 +34,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+
 @Mixin(CommandDispatcher.class)
 public abstract class CommandDispatcherMixin {
     @Inject(method = "register", at = @At("HEAD"), remap = false)
     private void register(LiteralArgumentBuilder<ServerCommandSource> command, CallbackInfoReturnable<LiteralCommandNode<ServerCommandSource>> cir) {
-        if (CustomCommandPermissionLevelRegistry.COMMAND_PERMISSION_MAP.containsKey(command.getLiteral())) {
+        if (
+            !Objects.equals(AmsServerSettings.commandCustomCommandPermissionLevel, "false") &&
+            CustomCommandPermissionLevelRegistry.COMMAND_PERMISSION_MAP.containsKey(command.getLiteral())
+        ) {
             int level = CustomCommandPermissionLevelRegistry.COMMAND_PERMISSION_MAP.get(command.getLiteral());
             command.requires(source -> source.hasPermissionLevel(level));
         }
