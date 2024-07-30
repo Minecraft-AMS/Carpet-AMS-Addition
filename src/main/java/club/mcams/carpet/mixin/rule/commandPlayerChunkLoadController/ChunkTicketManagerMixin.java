9fcf47ff961c81844d4cfc30b907b12ea9e95564
wendavid552 <david.jianwei.wen@gmail.com>
2024-07-30 08:33:38 +0800

    :bug: 修复原有控制玩家周围区域加载命令 修改了命令实现，现在通过修改玩家给附近区块的加载等级实现

--- src/main/java/club/mcams/carpet/mixin/rule/commandPlayerChunkLoadController/ChunkTicketManagerMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/commandPlayerChunkLoadController/ChunkTicketManagerMixin.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the Carpet AMS Addition project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2024 A Minecraft Server and contributors
+ *
+ * Carpet AMS Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpet AMS Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpet AMS Addition. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package club.mcams.carpet.mixin.rule.commandPlayerChunkLoadController;
 
 import club.mcams.carpet.AmsServerSettings;
@@ -11,7 +31,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ChunkTicketManager.class)
-public class ChunkTicketManagetMixin {
+public class ChunkTicketManagerMixin {
+    //#if MC>=11800
     @WrapOperation(
             method = "handleChunkEnter(Lnet/minecraft/util/math/ChunkSectionPos;Lnet/minecraft/server/network/ServerPlayerEntity;)V",
             at = @At(
@@ -26,4 +47,5 @@ public class ChunkTicketManagetMixin {
         }
         return level;
     }
+    //#endif
 }
