8193f6aac33157863c50bffeff49d7e959ffa82f
1024_byteeeee <2270484921@qq.com>
2024-12-14 12:21:29 +0800

    :art: 调整代码

--- src/main/java/club/mcams/carpet/mixin/rule/sensibleEnderman/PickUpBlockGoalMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/sensibleEnderman/PickUpBlockGoalMixin.java
@@ -18,24 +18,17 @@
  * along with Carpet AMS Addition.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package club.mcams.carpet.mixin.rule.sensibleEnderman_endermanPickUpDisabled;
+package club.mcams.carpet.mixin.rule.sensibleEnderman;
 
 import club.mcams.carpet.AmsServerSettings;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Block;
-//#if MC>=11904
-//$$ import net.minecraft.registry.tag.TagKey;
-//#elseif MC>=11800 && MC<11900
 import net.minecraft.tag.TagKey;
-//#else
-//$$ import net.minecraft.tag.Tag;
-//#endif
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -46,53 +39,15 @@ public abstract class PickUpBlockGoalMixin {
         method = "tick()V",
         at = @At(
             value = "INVOKE",
-            target =
-            //#if MC<11700
-            //$$ "Lnet/minecraft/block/Block;isIn(Lnet/minecraft/tag/Tag;)Z"
-            //#elseif MC<11904 && MC>=11800
-            "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/tag/TagKey;)Z"
-            //#elseif MC<11904
-            //$$ "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/tag/Tag;)Z"
-            //#else
-            //$$ "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"
-            //#endif
+            target = "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/tag/TagKey;)Z"
         )
     )
-    private boolean isBlockInTag(
-         //#if MC<11700
-         //$$ Block block,
-         //#else
-         BlockState blockState,
-         //#endif
-         //#if MC>=11800 && MC<11900
-         TagKey<Block> tag,
-         //#elseif MC<11904
-         //$$ Tag<Block> tag,
-         //#else
-         //$$ TagKey<Block> tag,
-         //#endif
-         Operation<Boolean> original
-    ) {
+    private boolean isBlockInTag(BlockState blockState, TagKey<Block> tag, Operation<Boolean> original) {
         if (AmsServerSettings.sensibleEnderman) {
-            //#if MC>=11700
             Block block = blockState.getBlock();
-            //#endif
-            return block.equals(Blocks.MELON) || block.equals(Blocks.CARVED_PUMPKIN);
+            return block.equals(Blocks.MELON) || block.equals(Blocks.PUMPKIN);
         } else {
-            //#if MC>=11700
             return original.call(blockState, tag);
-            //#else
-            //$$ return original.call(block, tag);
-            //#endif
-        }
-    }
-
-    @ModifyReturnValue(method = "canStart", at = @At("RETURN"))
-    private boolean canStart(boolean original) {
-        if (AmsServerSettings.endermanPickUpDisabled) {
-            return false;
-        } else {
-            return original;
         }
     }
 }
