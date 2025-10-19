0198dc9aae253b5657064d620e673470bb2f4dd8
1024-byteeeee <2270484921@qq.com>
2025-10-19 23:05:12 +0800

    检查AMS网络协议是否启用的开关

---
+++ src/main/java/club/mcams/carpet/observers/rule/network/NetworkProtocolObserver.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the Carpet AMS Addition project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2025 A Minecraft Server and contributors
+ *
+ * Carpet AMS Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpet AMS Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpet AMS Addition. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package club.mcams.carpet.observers.rule.network;
+
+import carpet.settings.ParsedRule;
+import club.mcams.carpet.utils.Messenger;
+import club.mcams.carpet.AmsServerSettings;
+import club.mcams.carpet.translations.Translator;
+import club.mcams.carpet.settings.SimpleRuleObserver;
+
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Formatting;
+
+public class NetworkProtocolObserver extends SimpleRuleObserver<Object> {
+    private final Translator tr = new Translator("validator.amsNetworkProtocol");
+
+    @Override
+    public void onValueChange(ServerCommandSource source, ParsedRule<Object> rule, Object oldValue, Object newValue) {
+        if (!AmsServerSettings.amsNetworkProtocol) {
+            Messenger.tell(source, Messenger.formatting(tr.tr("need_enable_protocol", getRuleName(rule)), Formatting.YELLOW));
+        }
+    }
+}
