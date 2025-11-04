abf321747bcd282089150409ebe568561a34e383
1024_byteeeee <2270484921@qq.com>
2025-11-04 17:05:10 +0800

    修复 #199

---
+++ src/main/java/club/mcams/carpet/mixin/rule/strongLeash/FireworkRocketItemMixin.java
@@ -0,0 +1,11 @@
+package club.mcams.carpet.mixin.rule.strongLeash;
+
+import club.mcams.carpet.utils.compat.DummyClass;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import top.byteeeee.annotationtoolbox.annotation.GameVersion;
+
+@GameVersion(version = "Minecraft >= 1.21.6")
+@Mixin(DummyClass.class)
+public abstract class FireworkRocketItemMixin {}
