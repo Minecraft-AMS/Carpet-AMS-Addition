724e410db754e630339734b82cf80a355a4d5920
1024-byteeeee <2270484921@qq.com>
2026-01-04 19:33:34 +0800

    调整代码

--- src/main/java/club/mcams/carpet/mixin/rule/commandPlayerChunkLoadController/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/commandPlayerChunkLoadController/ThreadedAnvilChunkStorageMixin.java
@@ -31,11 +31,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class ThreadedAnvilChunkStorageMixin {
     @Inject(method = "doesNotGenerateChunks", at = @At("HEAD"), cancellable = true)
     private void doesNotGenerateChunks(ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
-        if (AmsServerSettings.commandPlayerChunkLoadController) {
+        if (!Objects.equals(AmsServerSettings.commandPlayerChunkLoadController, "false")) {
             String playerName = player.getName().getString();
             if (!ChunkLoading.onlinePlayerMap.getOrDefault(playerName, true)) {
                 cir.setReturnValue(true);
